ea616c4bfcddb8db3a36add1b2fe0e4a9483b336
Cannavaro Yogi <yogi@paydia.id>
2023-07-10 03:04:34 +0700

    Update loading content

--- vue-front/src/views/HomeView.vue
+++ vue-front/src/views/HomeView.vue
@@ -33,9 +33,6 @@
           <div class="spinner-grow text-info" role="status">
             <span class="sr-only">Loading...</span>
           </div>
-          <div class="spinner-grow text-light" role="status">
-            <span class="sr-only">Loading...</span>
-          </div>
           <div class="spinner-grow text-dark" role="status">
             <span class="sr-only">Loading...</span>
           </div>
