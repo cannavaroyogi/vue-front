5764c1cf34b8efcfbd690f8dc459c5960abb32c8
unknown <cannavaro.yogi@ainosi.co.id>
2023-07-10 19:27:21 +0700

    Update title modal detail

--- vue-front/src/components/ModalDetail.vue
+++ vue-front/src/components/ModalDetail.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <b-modal :id="product.code_product" title="BootstrapVue">
+      <b-modal :id="product.code_product" hide-footer size="xl" :title="product.name_product">
         <p class="my-4">{{ product.name_product }}</p>
       </b-modal>
     </div>
